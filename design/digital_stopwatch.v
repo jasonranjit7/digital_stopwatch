c3d1c1e2f3f5486c209207ada37128ab8e8c078c
Jason Ranjit <jasonranjit@gmail.com>
2026-06-17 06:04:52 -0500

    files

--- design/digital_stopwatch.v
+++ design/digital_stopwatch.v
@@ -1,6 +1,7 @@
 `include "tick_divider/tick_divider.v"
-module digital_stopwatch #(parameter TICK_DIV = 10^4)(input clk,
-                 input rst,
+//set divider parameter to 5x10^7 to make clock period 1s for fpga board of freq 50MHz
+module digital_stopwatch #(parameter TICK_DIV = 4)(input clk,
+                 input rst, tick_rst,
                  input start_stop,
                  input lap,
                  input clear,
@@ -10,11 +11,12 @@ module digital_stopwatch #(parameter TICK_DIV = 10^4)(input clk,
                  output reg [15:0] lap_count,
                  output reg [1:0] state_dbg
                 );
+
+ wire tick_en;
   
-  wire tick_en;
-  
+
   tick_divider #(.TICK_DIV(TICK_DIV)) tdr(.clk(clk),
-                                          .rst(rst),
+                                          .rst(tick_rst),
                                           .tick_en(tick_en)
                                          );
   
@@ -25,16 +27,14 @@ module digital_stopwatch #(parameter TICK_DIV = 10^4)(input clk,
   				   S_LAP_HELD = 2'b10;
   
   //state transition
-  
-  always@(posedge clk) begin
+  always@(posedge tick_en) begin
     if(rst)
       state <= S_STOPPED;
-    else if(tick_en)
+    else
       state <= nxt_state;
   end
   
   //transition logic
-  
   always@(*) begin
     case(state)
       S_STOPPED: 
@@ -47,7 +47,7 @@ module digital_stopwatch #(parameter TICK_DIV = 10^4)(input clk,
       
       S_RUNNING: 
         begin
-          if(!start_stop)
+          if(start_stop | clear)
             nxt_state = S_STOPPED;
           else if(lap)
             nxt_state = S_LAP_HELD;
@@ -57,10 +57,12 @@ module digital_stopwatch #(parameter TICK_DIV = 10^4)(input clk,
       
       S_LAP_HELD:
         begin
-          if(!start_stop)
+          if(start_stop|clear)
             nxt_state = S_STOPPED;
-          else if(!lap)
+          else if(lap)
             nxt_state = S_RUNNING;
+          else
+            nxt_state = S_LAP_HELD;
         end
       
       default: begin
@@ -70,43 +72,47 @@ module digital_stopwatch #(parameter TICK_DIV = 10^4)(input clk,
   end
   
   //counter
-  always@(posedge clk) begin
+  always@(posedge tick_en) begin
     if(rst| clear) begin
       time_count <= 16'd0;
       lap_count <= 16'd0;
     end
-    else if(running && tick_en) begin
-      time_count <= time_count + 1;
+    else begin
+      if(state == S_RUNNING && nxt_state == S_LAP_HELD) begin
+      	lap_count <= time_count + 1;
+      end
+      if(state == S_RUNNING || nxt_state == S_LAP_HELD)
+      	time_count <= time_count + 1;
     end
   end
   
   //output logic
   always@(*) begin
-    if(state == S_STOPPED) begin
-      state_dbg = S_STOPPED;
-      running = 0;
-      lap_active = 0;
-      time_count = 0;
-      lap_count = 0;
-    end
-    else if(state == S_RUNNING) begin
-      state_dbg = S_RUNNING;
-      running = 1;
-      lap_active = 0;
-      lap_count = 0;
-    end
-    else if(state == S_LAP_HELD) begin
-      state_dbg = S_LAP_HELD;
-      running = 1;
-      lap_active = 1;
-      lap_count = time_count;
-    end
-    else begin
-      state_dbg = 2'b11; //invalid state
-      running = 0;
-      lap_active = 0;
-    end
+    state_dbg = 2'b11; //invalid state
+    running = 0;
+    lap_active = 0;
+    case(state)
+      S_STOPPED: begin
+        state_dbg = S_STOPPED;
+        running = 0;
+        lap_active = 0;
+      end
+      S_RUNNING: begin
+        state_dbg = S_RUNNING;
+        running = 1;
+        lap_active = 0;
+      end
+      S_LAP_HELD: begin
+        state_dbg = S_LAP_HELD;
+        running = 1;
+        lap_active = 1;
+      end
+      default: begin
+        state_dbg = 2'b11; //invalid state
+        running = 0;
+        lap_active = 0;
+      end
+    endcase
   end
-  
   
 endmodule
--- design/tb_digital_stopwatch.v
+++ design/tb_digital_stopwatch.v
@@ -1,5 +1,5 @@
-module digital_stopwatch_tb #(parameter TICK_DIV = 4)();
-  reg clk,rst,start_stop,lap,clear;
+module digital_stopwatch_tb #(parameter TICK_DIV = 2)();
+  reg clk,rst,tick_rst,start_stop,lap,clear;
   wire running, lap_active;
   wire [15:0] time_count, lap_count;
   wire [1:0] state_dbg;
@@ -8,6 +8,7 @@ module digital_stopwatch_tb #(parameter TICK_DIV = 4)();
   //DUT instantiation
   digital_stopwatch #(.TICK_DIV(TICK_DIV)) DUT(.clk(clk),
                 .rst(rst),
+                .tick_rst(tick_rst),
                 .start_stop(start_stop),
                 .lap(lap),
                 .clear(clear),
@@ -18,42 +19,98 @@ module digital_stopwatch_tb #(parameter TICK_DIV = 4)();
                 .state_dbg(state_dbg)
                );
   
-  //clk gen
+  wire tick = DUT.tick_en;
+  
   initial begin
     clk = 0;
+    rst = 1;
+    tick_rst = 1;
+    start_stop = 0;
+    lap = 0;
+    clear = 0;
+  end
+  
+  //clk gen
+  initial begin
     forever #20 clk = ~clk;
   end
   
-  //task for running
-  task run;
-    input s;
-    input l;
-    input c;
+  task start;
     begin
-      start_stop = s;
-      lap = l;
-      clear = c;
-      repeat(10)@(posedge clk);
+      @(negedge tick);
+      $display("Starting stopwatch");
+      #1 start_stop = 1;
+      @(posedge tick);
+      #1 start_stop = 0;
+    end
+  endtask
+
+  task clearing;
+    begin
+      @(negedge tick);
+      $display("Clearing stopwatch");
+      #1 clear = 1;
+      @(posedge tick);
+      #1 clear = 0;
+    end
+  endtask
+  
+  task lapping;
+    begin
+      @(negedge tick);
+      $display("pressing lap button");
+      #1 lap = 1;
+      @(posedge tick);
+      #1 lap = 0;
+    end
+  endtask
+  
+  task wait_ticks(input integer n);
+    begin
+      repeat(n*TICK_DIV) @(posedge clk);
+    end
+  endtask
+  
+  task test_start;
+    begin
+      start();
+      if(state_dbg == 2'b01)
+        $display("Inside running state");
+      wait_ticks(5);
+      lapping();
+      if(state_dbg == 2'b10)
+        $display("Inside lap mode");
+      wait_ticks(11);
+      lapping();
+      if(state_dbg == 2'b01)
+        $display("Inside running state");
+      wait_ticks(8);
+      lapping();
+      if(state_dbg == 2'b10)
+        $display("Inside lap mode");
+      wait_ticks(11);
+      clearing();
+      if(state_dbg == 2'b00)
+        $display("Inside idle state");
+      wait_ticks(5);
     end
   endtask
   
   initial begin
     $dumpfile("stopwatch.vcd");
     $dumpvars(0,digital_stopwatch_tb.DUT);
-    rst = 1;
-    repeat(5)@(posedge clk);
+    
+    $monitor("running = %b, state = %d, lap_count = %b, time_count = %0d",DUT.running, state_dbg, lap_count, time_count);
+    
+    wait_ticks(5);
+    tick_rst = 0;
+    wait_ticks(10);
     rst = 0;
-    $monitor("running = %b, state = %b, lap active =%b, clear = %b, lap_count = %b, time = %0t",DUT.running, state_dbg, lap_active, clear, lap_count, $time);
-    run(1,0,0);
-    run(1,0,0);
-    run(1,0,1);
-    run(1,0,0);
-    run(1,0,0);
-    run(1,1,0);
-    run(0,1,0);
+    wait_ticks(10);
+    test_start();
+    wait_ticks(5);
     $finish();
   end
       
   
 endmodule
-
